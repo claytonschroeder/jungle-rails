323b83191e93a2f8957d42ba8145e25cf5aa59e0
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-12-10 05:26:44 +0000

    saving reviews to database and showing on page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,19 @@ class ReviewsController < ApplicationController
 
 
 def create
-  raise "Yay, I'm here!"
+  @product = Product.find(params[:product_id])
+  @review = @product.reviews.create(review_params)
+  @review.user = current_user
+
+  if @review.save
+    redirect_to product_path(@product)
+  else
+    redirect_to '/'
+  end
 end
 
+private
+def review_params
+    params.require(:review).permit(:description, :rating)
+  end
 end
