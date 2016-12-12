7305e60ca3fa00e3c263259c04a4315a6e22b2ce
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-12-12 18:34:41 +0000

    formatted email to display html similar to order and cart page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'no-reply@jungle.com'
 
   def welcome_email(order, item, product)
     @order = order
     @item = item
     @product = product
-    mail(to: 'clytn7@gmail.com', subject: "order number #{@order.id}")
+    mail(to: 'clytn7@gmail.com', subject: "Order Number #{@order.id}")
   end
 end
